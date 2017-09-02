195a6fd5c554865a5fdb922fa3fb886fceecaa9c
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:36:10 +0100

    Initial import

---
+++ OneSlot.h
@@ -0,0 +1,29 @@
+#ifndef _ONESLOT_H
+#define _ONESLOT_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t OneSlot10[];
+    extern const uint8_t OneSlot11[];
+    extern const uint8_t OneSlot12[];
+    extern const uint8_t OneSlot13[];
+    extern const uint8_t OneSlot14[];
+    extern const uint8_t OneSlot15[];
+    extern const uint8_t OneSlot16[];
+    extern const uint8_t OneSlot17[];
+    extern const uint8_t OneSlot18[];
+    extern const uint8_t OneSlot19[];
+    extern const uint8_t OneSlot20[];
+    extern const uint8_t OneSlot21[];
+    extern const uint8_t OneSlot22[];
+    extern const uint8_t OneSlot23[];
+    extern const uint8_t OneSlot24[];
+    extern const uint8_t OneSlot25[];
+    extern const uint8_t OneSlot26[];
+    extern const uint8_t OneSlot27[];
+    extern const uint8_t OneSlot28[];
+    extern const uint8_t OneSlot29[];
+    extern const uint8_t OneSlot30[];
+};
+#endif
